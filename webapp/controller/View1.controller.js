32c548d17b66bec47b3fd3ffcbecd257fea53708
S0011259192 <js.piccirillo@ynoveo.fr>
2018-12-23 12:01:39 +0000

    Initialisation des fichiers de langue Configuration pour permettre de lancer l'app depuis webide Init de la vue dataset_list Ajout d'un bouton de test sur la page d'accueil de l'appli

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,9 +1,39 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
 	"use strict";
-
 	return Controller.extend("YNV.DSM.controller.View1", {
-
+		/**
+		 *@memberOf YNV.DSM.controller.View1
+		 */
+		action: function (oEvent) {
+			var that = this;
+			var actionParameters = JSON.parse(oEvent.getSource().data("wiring").replace(/'/g, "\""));
+			var eventType = oEvent.getId();
+			var aTargets = actionParameters[eventType].targets || [];
+			aTargets.forEach(function (oTarget) {
+				var oControl = that.byId(oTarget.id);
+				if (oControl) {
+					var oParams = {};
+					for (var prop in oTarget.parameters) {
+						oParams[prop] = oEvent.getParameter(oTarget.parameters[prop]);
+					}
+					oControl[oTarget.action](oParams);
+				}
+			});
+			var oNavigation = actionParameters[eventType].navigation;
+			if (oNavigation) {
+				var oParams = {};
+				(oNavigation.keys || []).forEach(function (prop) {
+					oParams[prop.name] = encodeURIComponent(JSON.stringify({
+						value: oEvent.getSource().getBindingContext(oNavigation.model).getProperty(prop.name),
+						type: prop.type
+					}));
+				});
+				if (Object.getOwnPropertyNames(oParams).length !== 0) {
+					this.getOwnerComponent().getRouter().navTo(oNavigation.routeName, oParams);
+				} else {
+					this.getOwnerComponent().getRouter().navTo(oNavigation.routeName);
+				}
+			}
+		}
 	});
 });
